6d71b23ad748d1907901e183e39649c7c13c32d5
statfs <statfs@163.com>
2020-11-04 14:26:53 +0800

    update setup version

--- setup.py
+++ setup.py
@@ -2,15 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name="aliyun-api-gateway-sign-py3",
-    version="1.0.2",
+    version="1.0.4",
     keywords=("pip", "aliyun", "gateway", "aliyun-gateway"),
     description="aliyun api网关调用python3",
     long_description="aliyun api gateway for python3",
     license="MIT Licence",
 
-    url="https://github.com/jnchenfeng/aliyun_api_gateway_sign_py3",
-    author="ChenFeng",
-    author_email="jnchenfeng123@163.com",
+    url="https://github.com/statfs/aliyun_api_gateway_sign_py3",
+    author="statfs",
+    author_email="statfs@163.com",
 
     packages=find_packages(),
     include_package_data=True,
